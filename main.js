547dc97a137cf694aea8cbc441bc80cd5bb80217
Federico Geretto <fede.geretto96@gmail.com>
2023-04-05 01:12:37 -0300

    faltan textos

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 // ABRIR Y CERRAR MENÚ
-
 const abrirMenu = document.querySelector("#abrir-menu");
 const cerrarMenu = document.querySelector("#cerrar-menu");
 const navegacion = document.querySelector("#navegacion")
-
-console.log(abrirMenu);
-console.log(cerrarMenu);
-console.log(navegacion);
 
 abrirMenu.addEventListener("click", () => {
   navegacion.classList.add("activo")
@@ -21,55 +16,20 @@ cerrarMenu.addEventListener("click", () => {
 })
 
 document.addEventListener("click", (event) => {
-  // Verifica si el elemento que se hizo clic está dentro del menú
   if (!navegacion.contains(event.target) && !abrirMenu.contains(event.target)) {
       navegacion.classList.remove("activo");
   }
 });
 
 const enlacesMenu = document.querySelectorAll(".navegacion li a");
-console.log(enlacesMenu);
 const arrayEnlacesMenu = Array.from(enlacesMenu);
-const arrayEnlacesMenuCerrar = arrayEnlacesMenu.slice(0, 3)
-arrayEnlacesMenuCerrar.forEach((enlace) => {
+arrayEnlacesMenu.forEach((enlace) => {
   enlace.addEventListener("click", () => {
       navegacion.classList.remove("activo");
   });
 });
 
-// abrirMenu.addEventListener("click", () => {
-//     navBar.classList.add("activo");
-// });
-
-// cerrarMenu.addEventListener("click", () => {
-//     navBar.classList.add("cerrar");
-
-//     setTimeout(() => {
-//         navBar.classList.remove("activo");
-//         navBar.classList.remove("cerrar");
-//     }, 300)
-// });
-
-// document.addEventListener("click", (event) => {
-//   // Verifica si el elemento que se hizo clic está dentro del menú
-//   if (!navBar.contains(event.target) && !abrirMenu.contains(event.target)) {
-//       navBar.classList.remove("activo");
-//   }
-// });
-
-// const enlacesMenu = document.querySelectorAll("#navbar li a");
-// console.log(enlacesMenu);
-// const arrayEnlacesMenu = Array.from(enlacesMenu);
-// const arrayEnlacesMenuCerrar = arrayEnlacesMenu.slice(0, 3)
-// arrayEnlacesMenuCerrar.forEach((enlace) => {
-//   enlace.addEventListener("click", () => {
-//       navBar.classList.remove("activo");
-//   });
-// });
-
-
 // ESCRIBIR PALABRAS
-
 const palabras = ["Landing Page", "Ecommerce", "Responsive", "Personalizado"];
 const maquinaDeEscribir = document.getElementById("maquina-de-escribir");
 
