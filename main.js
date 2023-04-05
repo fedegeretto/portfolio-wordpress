4c3c5d1e309ac832d7c01ec1708747201e64214f
Federico Geretto <fede.geretto96@gmail.com>
2023-04-05 01:58:42 -0300

    Portfolio

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
-// ABRIR Y CERRAR MENÚ
-const abrirMenu = document.querySelector("#abrir-menu");
-const cerrarMenu = document.querySelector("#cerrar-menu");
-const navegacion = document.querySelector("#navegacion")
-
-abrirMenu.addEventListener("click", () => {
-  navegacion.classList.add("activo")
-})
-
-cerrarMenu.addEventListener("click", () => {
-  navegacion.classList.add("cerrar")
-  setTimeout(() => {
-    navegacion.classList.remove("activo")
-    navegacion.classList.remove("cerrar")
-  }, 300);
-})
-
-document.addEventListener("click", (event) => {
-  if (!navegacion.contains(event.target) && !abrirMenu.contains(event.target)) {
-      navegacion.classList.remove("activo");
-  }
-});
-
-const enlacesMenu = document.querySelectorAll(".navegacion li a");
-const arrayEnlacesMenu = Array.from(enlacesMenu);
-arrayEnlacesMenu.forEach((enlace) => {
-  enlace.addEventListener("click", () => {
-      navegacion.classList.remove("activo");
-  });
-});
-
 // ESCRIBIR PALABRAS
-const palabras = ["Landing Page", "Ecommerce", "Responsive", "Personalizado"];
+const palabras = ["Landing Page", "Ecommerce", "Institucional", "Personalizado"];
 const maquinaDeEscribir = document.getElementById("maquina-de-escribir");
 
 function escribirPalabra(palabra) {
