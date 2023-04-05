f2f97ca7b7b86e986ce970f9a0d217c48e414539
Federico Geretto <fede.geretto96@gmail.com>
2023-04-05 00:44:29 -0300

    Portfolio final

--- main.js
+++ main.js
@@ -1,41 +1,71 @@
 // ABRIR Y CERRAR MENÚ
 
 const abrirMenu = document.querySelector("#abrir-menu");
-
 const cerrarMenu = document.querySelector("#cerrar-menu");
+const navegacion = document.querySelector("#navegacion")
 
-const navBar = document.querySelector("#navbar");
-
+console.log(abrirMenu);
+console.log(cerrarMenu);
+console.log(navegacion);
 
 abrirMenu.addEventListener("click", () => {
-    navBar.classList.add("activo");
-});
+  navegacion.classList.add("activo")
+})
 
 cerrarMenu.addEventListener("click", () => {
-    navBar.classList.add("cerrar");
-
-    setTimeout(() => {
-        navBar.classList.remove("activo");
-        navBar.classList.remove("cerrar");
-    }, 300)
-});
+  navegacion.classList.add("cerrar")
+  setTimeout(() => {
+    navegacion.classList.remove("activo")
+    navegacion.classList.remove("cerrar")
+  }, 300);
+})
 
 document.addEventListener("click", (event) => {
   // Verifica si el elemento que se hizo clic está dentro del menú
-  if (!navBar.contains(event.target) && !abrirMenu.contains(event.target)) {
-      navBar.classList.remove("activo");
+  if (!navegacion.contains(event.target) && !abrirMenu.contains(event.target)) {
+      navegacion.classList.remove("activo");
   }
 });
 
-const enlacesMenu = document.querySelectorAll("#navbar li a");
+const enlacesMenu = document.querySelectorAll(".navegacion li a");
 console.log(enlacesMenu);
 const arrayEnlacesMenu = Array.from(enlacesMenu);
 const arrayEnlacesMenuCerrar = arrayEnlacesMenu.slice(0, 3)
 arrayEnlacesMenuCerrar.forEach((enlace) => {
   enlace.addEventListener("click", () => {
-      navBar.classList.remove("activo");
+      navegacion.classList.remove("activo");
   });
 });
+
+// abrirMenu.addEventListener("click", () => {
+//     navBar.classList.add("activo");
+// });
+
+// cerrarMenu.addEventListener("click", () => {
+//     navBar.classList.add("cerrar");
+
+//     setTimeout(() => {
+//         navBar.classList.remove("activo");
+//         navBar.classList.remove("cerrar");
+//     }, 300)
+// });
+
+// document.addEventListener("click", (event) => {
+//   // Verifica si el elemento que se hizo clic está dentro del menú
+//   if (!navBar.contains(event.target) && !abrirMenu.contains(event.target)) {
+//       navBar.classList.remove("activo");
+//   }
+// });
+
+// const enlacesMenu = document.querySelectorAll("#navbar li a");
+// console.log(enlacesMenu);
+// const arrayEnlacesMenu = Array.from(enlacesMenu);
+// const arrayEnlacesMenuCerrar = arrayEnlacesMenu.slice(0, 3)
+// arrayEnlacesMenuCerrar.forEach((enlace) => {
+//   enlace.addEventListener("click", () => {
+//       navBar.classList.remove("activo");
+//   });
+// });
 
 
 // ESCRIBIR PALABRAS
