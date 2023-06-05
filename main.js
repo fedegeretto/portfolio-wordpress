58aed6144c38728fc9510d469ed765a3f217183f
Federico Geretto <fede.geretto96@gmail.com>
2023-06-05 12:30:14 -0300

    Agregando detalles

--- main.js
+++ main.js
@@ -25,3 +25,17 @@ function escribirSiguientePalabra() {
 }
 
 escribirSiguientePalabra();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var mobileLink = document.getElementById('mobile-link');
+
+  mobileLink.addEventListener('click', function(event) {
+    if (window.innerWidth <= 768) { // Verificar si es la versión mobile (puedes ajustar el ancho según tus necesidades)
+      event.preventDefault(); // Prevenir la acción por defecto del enlace
+
+      setTimeout(function() {
+        window.location.href = mobileLink.getAttribute('href'); // Redirigir después de 2 segundos (puedes ajustar el tiempo según tus necesidades)
+      }, 2000);
+    }
+  });
+});
