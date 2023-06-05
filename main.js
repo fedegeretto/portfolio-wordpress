5dc52eddaf88881a06cc3876af733db9b4438972
Federico Geretto <fede.geretto96@gmail.com>
2023-06-05 20:57:45 -0300

    Portfolio final

--- main.js
+++ main.js
@@ -25,19 +25,3 @@ function escribirSiguientePalabra() {
 }
 
 escribirSiguientePalabra();
-
-function openModal(image) {
-  var modal = document.getElementById('modal');
-  var modalImage = document.getElementById('modal-image');
-  
-  modalImage.src = image.src;
-  modal.style.display = 'block';
-}
-
-function closeModal() {
-  var modal = document.getElementById('modal');
-  var modalImage = document.getElementById('modal-image');
-
-  modalImage.src = '';
-  modal.style.display = 'none';
-}
