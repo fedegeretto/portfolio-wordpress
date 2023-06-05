6918f76192ef5d7dcc8e5d0d7bd8842ff669ca77
Federico Geretto <fede.geretto96@gmail.com>
2023-06-05 13:00:54 -0300

    aisdjia

--- main.js
+++ main.js
@@ -26,16 +26,27 @@ function escribirSiguientePalabra() {
 
 escribirSiguientePalabra();
 
-document.addEventListener('DOMContentLoaded', function() {
-  var mobileLink = document.getElementById('mobile-link');
 
-  mobileLink.addEventListener('click', function(event) {
-    if (window.innerWidth <= 768) { // Verificar si es la versión mobile (puedes ajustar el ancho según tus necesidades)
-      event.preventDefault(); // Prevenir la acción por defecto del enlace
 
+  // Obtener el contenedor de imagen y el enlace
+  var imageContainer = document.querySelector('.image-container');
+  var link = document.getElementById('mobile-link');
+
+  // Variable para controlar el estado del enlace
+  var linkActivated = false;
+
+  // Función para activar el enlace después de 3 segundos
+  function activateLink() {
+    if (!linkActivated) {
+      linkActivated = true;
       setTimeout(function() {
-        window.location.href = mobileLink.getAttribute('href'); // Redirigir después de 2 segundos (puedes ajustar el tiempo según tus necesidades)
-      }, 2000);
+        link.style.pointerEvents = 'auto'; // Habilitar el enlace
+      }, 3000); // Retrasar la activación en 3 segundos (3000 ms)
     }
-  });
-});
+  }
+
+  // Agregar eventos de hover y touchstart al contenedor de imagen
+  imageContainer.addEventListener('mouseover', activateLink);
+  imageContainer.addEventListener('touchstart', activateLink);
+
+
